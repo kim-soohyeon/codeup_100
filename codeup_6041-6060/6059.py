f2dafec5de96110f5546863a7f1a1e38ea65ad1f
kim-soohyeon <kimsh1691@gmail.com>
2021-04-21 22:16:15 +0900

    기초-비트단위논리연산

---
+++ codeup_6041-6060/6059.py
@@ -0,0 +1,14 @@
+# https://codeup.kr/problem.php?id=6059
+# 6059 : [기초-비트단위논리연산] 비트단위로 NOT 하여 출력하기
+
+# 입력 된 정수를 비트단위로 참/거짓을 바꾼 후 정수로 출력해보자.
+# 비트단위(bitwise)연산자 ~ 를 붙이면 된다.(~ : tilde, 틸드라고 읽는다.)
+
+# ** 비트단위(bitwise) 연산자는,
+# ~(bitwise not), &(bitwise and), |(bitwise or), ^(bitwise xor),
+# <<(bitwise left shift), >>(bitwise right shift)
+# 가 있다.
+
+a = int(input())
+
+print(~a)
