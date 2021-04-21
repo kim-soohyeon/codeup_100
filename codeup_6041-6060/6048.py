7e7b03d863bf62a953e39f59eed6ab64ed77a260
kim-soohyeon <kimsh1691@gmail.com>
2021-04-21 21:47:16 +0900

    기초-비교연산

---
+++ codeup_6041-6060/6048.py
@@ -0,0 +1,13 @@
+# https://codeup.kr/problem.php?id=6048
+# 6048 : [기초-비교연산] 정수 2개 입력받아 비교하기1
+
+# 두 정수(a, b)를 입력받아
+# a가 b보다 작으면 True 를, a가 b보다 크거나 같으면 False 를 출력하는 프로그램을 작성해보자.
+
+a,b = map(int,input().split())
+
+if a<b:
+    print('True')
+else:
+    print('False')
+
