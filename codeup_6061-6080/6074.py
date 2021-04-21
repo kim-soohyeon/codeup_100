fdde7f09a671154b5de888438d3229366497ddb5
kim-soohyeon <kimsh1691@gmail.com>
2021-04-21 23:31:01 +0900

    기초-반복실행구조

---
+++ codeup_6061-6080/6074.py
@@ -0,0 +1,13 @@
+# https://codeup.kr/problem.php?id=6074
+# 6074 : [기초-반복실행구조] 문자 1개 입력받아 알파벳 출력하기
+
+# 영문 소문자(a ~ z) 1개가 입력되었을 때,
+# a부터 그 문자까지의 알파벳을 순서대로 출력해보자.
+
+
+c = ord(input())
+t = ord('a')
+
+while t<=c:
+    print(chr(t), end=' ')
+    t += 1
---
+++ codeup_6061-6080/6075.py
@@ -0,0 +1,14 @@
+# https://codeup.kr/problem.php?id=6075
+# 6075 : [기초-반복실행구조] 정수 1개 입력받아 그 수까지 출력하기1
+
+# 정수(0 ~ 100) 1개를 입력받아 0부터 그 수까지 순서대로 출력해보자.
+
+a=int(input())
+
+i=0
+while i<=a:
+   print(i)
+   i+=1
+
+# for i in range(a+1):
+#     print(i)
