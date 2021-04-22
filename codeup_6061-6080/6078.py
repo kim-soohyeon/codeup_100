af9d395d599d53ff68ee9daf47f31f75200752ff
kim-soohyeon <kimsh1691@gmail.com>
2021-04-22 14:12:05 +0900

    기초-종합

---
+++ codeup_6061-6080/6078.py
@@ -0,0 +1,16 @@
+# https://codeup.kr/problem.php?id=6078
+# 6078 : [기초-종합] 원하는 문자가 입력될 때까지 반복 출력하기
+
+# 영문 소문자 'q'가 입력될 때까지
+# 입력한 문자를 계속 출력하는 프로그램을 작성해보자.
+
+
+
+while(1):
+   a = input()
+   print(a)
+   if a=="q":
+      break
+   else:
+      continue
+
